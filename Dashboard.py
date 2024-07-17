54433c6f41a0a3944dac8dcf1a5308b0dec3c08c
Balto86 <159461282+Balto86@users.noreply.github.com>
2024-07-17 14:31:28 -0500

    deber semana 8

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,5 @@
 import os
 
-
 def mostrar_codigo(ruta_script):
     # Asegúrate de que la ruta al script es absoluta
     ruta_script_absoluta = os.path.abspath(ruta_script)
@@ -10,13 +9,16 @@ def mostrar_codigo(ruta_script):
             print(archivo.read())
     except FileNotFoundError:
         print("El archivo no se encontró.")
+    except IsADirectoryError:
+        print("La ruta especificada es un directorio.")
+    except IOError as e:
+        print(f"Ocurrió un error de E/S al leer el archivo: {e}")
     except Exception as e:
-        print(f"Ocurrió un error al leer el archivo: {e}")
-
+        print(f"Ocurrió un error inesperado al leer el archivo: {e}")
 
 def mostrar_menu():
     # Define la ruta base donde se encuentra el dashboard.py
-    ruta_base = os.path.dirname(__file__)
+    ruta_base = os.path.dirname(os.path.abspath(__file__))
 
     opciones = {
         '1': 'Unidad 1/1.2. Tecnicas de Programacion/1.2-1. Ejemplo Tecnicas de Programacion.py',
@@ -39,7 +41,6 @@ def mostrar_menu():
             mostrar_codigo(ruta_script)
         else:
             print("Opción no válida. Por favor, intenta de nuevo.")
-
 
 # Ejecutar el dashboard
 if __name__ == "__main__":
